558f370a1fbee61b41185b6cb51be899f12eb69e
khushboo7599 <65671190+khushboo7599@users.noreply.github.com>
2021-07-08 08:39:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
+
+app.post('/message',function(req,res){
+  console.log(req);
+  console.log("Reply Body:"+req.body);
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
